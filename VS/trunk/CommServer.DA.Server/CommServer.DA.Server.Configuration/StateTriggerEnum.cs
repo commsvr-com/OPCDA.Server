3d702a8b0223e3691bc270f41edfca685999e542
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2016-02-17 20:44:32 +0000

    Architecture improvements - working on PR24-Bublioteka and move NetworkConfigLib => CommServer.DA.Server.Configuration Issue #: 4772

---
+++ VS/trunk/CommServer.DA.Server/CommServer.DA.Server.Configuration/StateTriggerEnum.cs
@@ -0,0 +1,39 @@
+﻿//<summary>
+//  Title   : State Trigger Enum
+//  System  : Microsoft Visual C# .NET
+//  $LastChangedDate$
+//  $Rev$
+//  $LastChangedBy$
+//  $URL$
+//  $Id$
+//  History :
+//    20081006: mzbrzezny: created
+//
+//  Copyright (C)2008, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto:techsupp@cas.eu
+//  http://www.cas.eu
+//</summary>
+
+
+namespace CAS.NetworkConfigLib
+{
+  /// <summary>
+  /// State Triggers Enum
+  /// </summary>
+  public enum StateTrigger: sbyte
+  {
+    /// <summary>
+    /// None trigger
+    /// </summary>
+    None = 0,
+    /// <summary>
+    /// State high trigger
+    /// </summary>
+    StateHigh = 1,
+    /// <summary>
+    /// State low trigger
+    /// </summary>
+    StateLow = 2
+  }
+}
