2c51636843ae77e240c01544bdf53299dc1f7b8b
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2016-09-02 09:21:32 +0000

    CommServer.DA.Server - remove direct dependencies - working in progress - started rearrangement of files and cleanup the namespaces. Issue #: 4859

--- VS/trunk/CommServer.DA.Server/ProtocolHub.CommunicationUnitTests/TestRetryFilter.cs
+++ VS/trunk/CommServer.DA.Server/ProtocolHub.CommunicationUnitTests/TestRetryFilter.cs
@@ -13,9 +13,9 @@
 //  http://www.cas.eu
 //_______________________________________________________________
 
-using CAS.CommServer.ProtocolHub.Communication;
+using CAS.CommServer.ProtocolHub.Communication.BaseStation;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+using System.Diagnostics;
 
 namespace CAS.CommServer.ProtocolHub.CommunicationUnitTests
 {
@@ -25,7 +25,7 @@ namespace CAS.CommServer.ProtocolHub.CommunicationUnitTests
     private RetryFilter retryFilter = new RetryFilter(5);
 
     [TestMethod]
-    public void GoTest()
+    public void RetryFilterTestMethod()
     {
       Assert.AreEqual(retryFilter.Retry, 5, "Starting value check");
       Assert.AreEqual(retryFilter.Quality, 100.0);
@@ -34,24 +34,20 @@ namespace CAS.CommServer.ProtocolHub.CommunicationUnitTests
       Assert.AreEqual(retryFilter.Retry, 5, "Starting value check");
       Assert.AreEqual(retryFilter.Quality, 100.0);
       for (int i = 0; i < 10; i++)
-      {
-        //Console.WriteLine( "Retry = {0}", retryFilter.Retry );
         retryFilter.MarkFail();
-      }
       Assert.AreEqual(1, retryFilter.Retry, "Fail value check");
       Assert.IsTrue(retryFilter.Quality <= 10.0);
       Assert.IsTrue(retryFilter.Quality >= 0.0);
-      Console.WriteLine("Quality fail = {0}", retryFilter.Quality);
+      Debug.WriteLine("Quality fail = {0}", retryFilter.Quality);
       for (int i = 0; i < 10; i++)
       {
         retryFilter.MarkFail();
-        //Console.WriteLine( "Quality = {0}", retryFilter.Quality );
         retryFilter.MarkSuccess();
       }
       Assert.AreEqual(5, retryFilter.Retry, "Success value check");
       Assert.IsTrue(retryFilter.Quality <= 60.0);
       Assert.IsTrue(retryFilter.Quality >= 40.0);
-      Console.WriteLine("Quality poor = {0}", retryFilter.Quality);
+      Debug.WriteLine("Quality poor = {0}", retryFilter.Quality);
 
     }
   }
