8a289cd59613b638e10537278234d411b3c48603
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2017-06-07 20:40:34 +0000

    CommServer.DA.Server - removed deprecated AssemblyInfo.cs Issue #: 4862

--- VS/trunk/CommServer.DA.Server/Server.NETServer/Properties/AssemblyInfo.cs
+++ VS/trunk/CommServer.DA.Server/Server.NETServer/Properties/AssemblyInfo.cs
@@ -2,13 +2,13 @@
 //_______________________________________________________________
 //  Title   :  Assembly info for: CAS.CommServer.DA.Server.NETServer
 //  System  : Microsoft VisualStudio 2015 / C#
-//  $LastChangedDate:  $
-//  $Rev:  $
-//  $LastChangedBy: $
-//  $URL:  $
-//  $Id:  $
+//  $LastChangedDate$
+//  $Rev$
+//  $LastChangedBy$
+//  $URL$
+//  $Id$
 //
-//  Copyright (c) 2000-2016 CAS LODZ POLAND
+//  Copyright (c) 2017 CAS LODZ POLAND
 //  +48 (42) 686 25 47
 //  techsupp@cas.eu
 //  www.cas.eu
@@ -17,13 +17,14 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security.Permissions;
 
 [assembly: AssemblyTitle("CAS.CommServer.DA.Server.NETServer")]
 [assembly: AssemblyDescription("OPC DA Server Component")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CAS")]
 [assembly: AssemblyProduct("CAS CommServer Family")]
-[assembly: AssemblyCopyright("Copyright (c) 2000-2016 CAS LODZ POLAND")]
+[assembly: AssemblyCopyright("Copyright (c) 2017 CAS LODZ POLAND")]
 [assembly: AssemblyTrademark("CommServer")]
 [assembly: AssemblyCulture("")]
 [assembly: Guid("41E9B462-5C59-455a-909D-7C38A54DAC9A")]
@@ -39,5 +40,6 @@ using System.Runtime.InteropServices;
     Phone = "+48 42 686 25 47"
   )
 ]
-[assembly: InternalsVisibleToAttribute("CAS.CommServer.DA.Server.NETServerUnitTest", AllInternalsVisible = true)]
+[assembly: SecurityPermissionAttribute(SecurityAction.RequestMinimum, SerializationFormatter = true)]
+[assembly: InternalsVisibleToAttribute("CAS.CommServer.DA.Server.NETServer", AllInternalsVisible = true)]
 
