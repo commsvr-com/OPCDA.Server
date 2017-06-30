a2c51df30e28261415370a6dfc9a49171563dd52
Mariusz <mpostol@cas.eu>
2017-06-30 21:05:01 +0000

    CommServer.DA.Server - improved life-cycle management of Cache, Device, CommunicationHub classes. Issue #: 4862

---
+++ Server.NETServer/ToBeDisposedAfterShutdown.cs
@@ -0,0 +1,51 @@
+﻿//_______________________________________________________________
+//  Title   : ToBeDisposedAfterShutdown
+//  System  : Microsoft VisualStudio 2015 / C#
+//  $LastChangedDate$
+//  $Rev$
+//  $LastChangedBy$
+//  $URL$
+//  $Id$
+//
+//  Copyright (C) 2017, CAS LODZ POLAND.
+//  TEL: +48 608 61 98 99 
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//_______________________________________________________________
+
+using Opc;
+using System;
+
+namespace CAS.OpcSvr.Da.NETServer
+{
+  /// <summary>
+  /// Class ToBeDisposedAfterShutdown - provides <see cref="ServerShutdownEventHandler"/> to be used to dispose the object
+  /// </summary>
+  internal class ToBeDisposedAfterShutdown
+  {
+
+    /// <summary>
+    /// Creates <see cref="ToBeDisposedAfterShutdown"/> and returns the server shutdown event handler to be used to dispose <paramref name="toBeDisposed"/>.
+    /// </summary>
+    /// <param name="toBeDisposed">To be disposed.</param>
+    /// <returns>ServerShutdownEventHandler.</returns>
+    internal static ServerShutdownEventHandler GetServerShutdownEventHandler(IDisposable toBeDisposed)
+    {
+      ToBeDisposedAfterShutdown _this = new ToBeDisposedAfterShutdown((toBeDisposed));
+      return _this.ShutdowntEventHandler;
+    }
+
+    #region private
+    private ToBeDisposedAfterShutdown(IDisposable toBeDisposed)
+    {
+      m_ToBeDisposed = toBeDisposed;
+    }
+    private void ShutdowntEventHandler(string reason)
+    {
+      m_ToBeDisposed.Dispose();
+    }
+    private IDisposable m_ToBeDisposed;
+    #endregion
+
+  }
+}
---
+++ Server.NETServerUnitTest/ServerUnitTest.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CAS.CommServer.DA.Server.NETServer.UnitTest
+{
+  [TestClass]
+  public class ServerUnitTest
+  {
+
+    [TestMethod]
+    public void ConstructorTestMethod()
+    {
+     int _serverShutdownCount = 0;
+      using (CAS.OpcSvr.Da.NETServer.Server _serverInstance = new OpcSvr.Da.NETServer.Server(true))
+      {
+        _serverInstance.ServerShutdown += x => _serverShutdownCount++;
+      };
+      Assert.AreEqual<int>(0, _serverShutdownCount);
+    }
+  }
+}
