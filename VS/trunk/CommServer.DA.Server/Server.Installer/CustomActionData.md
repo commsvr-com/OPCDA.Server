0b2eb918d0962a25b768836d38492dcf6ef36b50
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2017-07-15 15:20:55 +0000

    CommServer.DA.Server - create installation package - moving license file from ProtocolHub to CommServer.DA.Server Issue #: 4862

---
+++ VS/trunk/CommServer.DA.Server/Server.Installer/CustomActionData.md
@@ -0,0 +1,7 @@
+# Install
+
+`/ca_user="[IT_USER]" /ca_company="[IT_COMPANY]" /ca_email="[IT_EMAIL]"`
+
+# Commit
+
+`/ca_manufacturer="[Manufacturer]" /ca_arphelplink="[ARPHELPLINK]" /ca_upgradecode="[UpgradeCode]" /ca_allusers="[ALLUSERS]" /ca_productname="[ProductName]" /ca_version="[ProductVersion]" /ca_user="[IT_USER]" /ca_company="[IT_COMPANY]" /ca_email="[IT_EMAIL]"`
--- VS/trunk/CommServer.DA.Server/Server.ProductInstaller.UnitTest/ProductInstallerUnitTest.cs
+++ VS/trunk/CommServer.DA.Server/Server.ProductInstaller.UnitTest/ProductInstallerUnitTest.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
 using System.Configuration.Install;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace CAS.CommServer.DA.Server.ProductInstaller.UnitTest
 {
@@ -14,11 +15,15 @@ namespace CAS.CommServer.DA.Server.ProductInstaller.UnitTest
     {
       using (InstallerCustomActions _customActions = new InstallerCustomActions())
       {
-        Installer[] _embededInstallers = new Installer[2];
-        _customActions.Installers.CopyTo(_embededInstallers, 0);
-        CollectionAssert.AllItemsAreNotNull(_embededInstallers);
-        List<string> names = _embededInstallers.Select<Installer, string>(x => x.ToString()).ToList<string>();
-        CollectionAssert.AreEqual(names, new string[]{ "CAS.Lib.CodeProtect.LibInstaller", "CAS.CommServer.ProtocolHub.Communication.CommServerInstaller" });
+        Installer[] _embeddedInstallers = new Installer[2];
+        _customActions.Installers.CopyTo(_embeddedInstallers, 0);
+        CollectionAssert.AllItemsAreNotNull(_embeddedInstallers);
+        List<string> _names = _embeddedInstallers.Select<Installer, string>(x => x.ToString()).ToList<string>();
+        CollectionAssert.AreEqual(_names, new string[]{ "CAS.Lib.CodeProtect.LibInstaller", "System.Diagnostics.EventLogInstaller" });
+        EventLogInstaller _EventLogInstaller = _embeddedInstallers[1] as EventLogInstaller;
+        Assert.IsNotNull(_EventLogInstaller);
+        Assert.AreEqual<string>("CAS.RealTime.Core", _EventLogInstaller.Source);
+        Assert.AreEqual<UninstallAction>(UninstallAction.Remove, _EventLogInstaller.UninstallAction);
       }
     }
   }
