9755d881ef96fbd15e002e14f4cef10ef279da67
Mariusz <mpostol@cas.eu>
2017-08-08 19:18:22 +0000

    CommServer.DA.Server - create installation package - CommServer.DA.Server released 4.2.6 prerelease version  - Added Server.ConfigurationEditor  - updated log files Issue #: 4862

---
+++ Server.ConfigurationEditor/Program.cs
@@ -0,0 +1,32 @@
+﻿using CAS.CommServer.ProtocolHub.ConfigurationEditor.HMI;
+using CAS.CommServer.ProtocolHub.ConfigurationEditor;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CAS.CommServer.DA.Server.ConfigurationEditor
+{
+  static class Program
+  {
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+      AssemblyTraceEvent.Tracer.TraceMessage(TraceEventType.Verbose, 32, "Starting application CAS.CommServer.DA.Server.ConfigurationEditor");
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new ProtocolHub.ConfigurationEditor.HMI.ConfigTreeView(ConfigurationManagement.ProtocolHubConfiguration,
+                             new ConfigIOHandler(ConfigurationManagement.ReadConfiguration),
+                             new ConfigIOHandler(ConfigurationManagement.SaveProc),
+                             new ConfigIOHandler(ConfigurationManagement.ClearConfig),
+                             false)
+                      );
+      AssemblyTraceEvent.Tracer.TraceMessage(TraceEventType.Verbose, 32, "Finishing application CAS.CommServer.DA.Server.ConfigurationEditor");
+    }
+  }
+}
---
+++ Server.Setup/SetupLogs/logdirOfDestinationFolders.cmd
@@ -0,0 +1,2 @@
+dir /s "C:\Program Files (x86)\CAS\CAS.CommServer" >"Program Files (x86).dir.log"
+dir /s "C:\ProgramData\CAS\CAS.CommServer" >"ProgramData.Dir.log"
