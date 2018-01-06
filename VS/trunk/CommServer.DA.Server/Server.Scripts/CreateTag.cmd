3696b88c1da8e7f21bcf9ae1d2461cc7bba62103
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2018-01-06 20:00:14 +0000

    CommServer.DA.Server - create installation package - create tag - created CreateTag.cmd - added keywords. Issue #: 4862

--- VS/trunk/CommServer.DA.Server/Server.Scripts/CreateTag.cmd
+++ VS/trunk/CommServer.DA.Server/Server.Scripts/CreateTag.cmd
@@ -1,9 +1,9 @@
 
-rem//  $LastChangedDate:  $
-rem//  $Rev:  $
-rem//  $LastChangedBy:  $
-rem//  $URL:  $
-rem//  $Id:  $
+rem//  $LastChangedDate$
+rem//  $Rev$
+rem//  $LastChangedBy$
+rem//  $URL$
+rem//  $Id$
 
 
 svn mkdir svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.DA.Server.rel_4_3_3  -m "created new svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.DA.Server.rel_4_3_3"
