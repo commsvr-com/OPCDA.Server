d70f7ee86daabc0190b260cbd95adfb8d9d3469b
Mariusz <mpostol@cas.eu>
2018-01-06 19:37:36 +0000

    CommServer.DA.Server - create installation package - create tag - working on TagCreation script - working copy Issue #: 4862

---
+++ Server.Scripts/CreateTagPrototyping.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CAS.CommServer.DA.Client.Scripts
+{
+
+  [TestClass]
+  public class CreateTagPrototyping
+  {
+
+    [TestMethod]
+    public void CreateTagTest()
+    {
+      string _version = "2.01.1061"; //random version for testing purpose only
+
+      #region Copy to script
+      string _rel = _version.Replace(".", "_"); // _version must be replaced by the correct version variable depending on the project.
+      string _repositoryUrl = @"svn://svnserver.hq.cas.com.pl/VS/";
+      string _solutionPath = @"/CommServer.DA.Client";
+      string _trunkPath = $@"{_repositoryUrl}trunk{_solutionPath}/";
+      string _tagPath = $@"{_repositoryUrl}tags{_solutionPath}.rel_{_rel}";
+      #endregion
+
+      Assert.AreEqual("svn://svnserver.hq.cas.com.pl/VS/trunk/CommServer.DA.Client/", _trunkPath);
+      Assert.AreEqual("svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.DA.Client.rel_2_01_1061", _tagPath);
+
+    }
+  }
+
+}
